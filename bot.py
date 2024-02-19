f19dae65cf058ba92be87b3375aa7dc9d6fa6c67
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2024-02-19 14:21:37 +0900

    load bot data

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ def get_delist_tokens(url):
 		options.add_argument("--remote-debugging-pipe")
 		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+		print("Scrape delisting page")
 		driver.get(url)
 
 		html_source = driver.page_source
@@ -49,8 +50,6 @@ def get_delist_tokens(url):
 						blacklist = f"{coin}/.*"
 						if not blacklist in tokens:
 							tokens.append(blacklist)
-					# article_tokens = match_result.group(1).split(",|&")
-					# tokens.extend(map(lambda elem: elem.strip(), article_tokens))
 				elif ("NOTICE OF REMOVAL OF " in title) and ("MARGIN" not in title) and (count_notice > 0):
 					count_notice -= 1
 					link = f"https://www.binance.com{link.get('href')}"
@@ -60,7 +59,6 @@ def get_delist_tokens(url):
 					lis = soup.find_all("p", class_p_list_coins)
 					for li in lis:
 						spans = li.find_all("span", "richtext-text")
-						exclude_in_span = ["<i>", "<strong>"]
 						for span in spans:
 							if "/" in span.text:
 								line = span.text.replace(": ", "")
@@ -68,23 +66,13 @@ def get_delist_tokens(url):
 								for coin in arr_coins:
 									if not coin in tokens:
 										tokens.append(coin)
-								# print(span.text)
-					# title = title.replace("BINANCE WILL DELIST ", "")
-					# arr_title = title.split(" ON ")
-					# arr_coins = arr_title[0].split(", ")
-					# for coin in arr_coins:
-					# 	blacklist = f"{coin}/.*"
-					# 	if not blacklist in tokens:
-					# 		tokens.append(blacklist)
 
 		driver.quit()
-		# print(tokens)
 		save_local_blacklist()
 		save_local_processed()
 	except Exception as e:
 		print("Failed to get article list.")
-		# print(e)
-	# return tokens
+		print(e)
 
 def open_local_blacklist():
 
@@ -189,27 +177,22 @@ def save_local_processed():
 	# for i in data['exchange']['pair_processed']:
 	# 	print(i)
 
-def loop():
-	print("Checking for delisted tokens...")
-	blacklisted_tokens = []
-	tokens = get_delist_tokens()
-	# for instance in bot_control.instances:
-	#     blacklist = bot_control.get_blacklist(instance)
-	#     if not blacklist:
-	#         continue
-	#     tokens_not_blacklisted = [
-	#         token
-	#         for token in tokens
-	#         if not any(bl_pair.split("/")[0].upper() == token for bl_pair in blacklist)
-	#     ]
-	#     for token in tokens_not_blacklisted:
-	#         bot_control.blacklist_pair(instance, f"{token}/.*")
-	#         blacklisted_tokens.append(token)
-	# blacklisted_tokens = list(set(blacklisted_tokens))
-	# if blacklisted_tokens:
-	#     print(f"Blacklisted {len(blacklisted_tokens)} tokens.")
+def load_bots_data():
+	file = open('bots.json')
+	data = json.load(file)
+	file.close()
+
+	for bot in data:
+		print(bot)
+		# if bot['ip_address'] == ip_address:
+		# 	username = bot['username']
+		# 	password = bot['password']
+		# 	break
+
+	# return username, password
 
 if __name__ == "__main__":
+	load_bots_data()
 	open_local_blacklist()
 	open_local_processed()
 	get_delist_tokens(url)
